cf420ee21a984641707dc0f669fc8485794e2f89
mayconsilva24 <sousamaycon83@gmail.com>
2024-11-09 18:20:58 -0300

    Initial commit

---
+++ src/components/category/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "@/styles/colors";
+
+export const styles = StyleSheet.create({
+    conteiner: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: "600",
+    },
+})
